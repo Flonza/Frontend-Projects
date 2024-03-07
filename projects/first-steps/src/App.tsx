676a0fd036ead35b6a88fe04fc63ef1b25ecd5e4
Never <jose.mendoza@sisloga.com>
2024-03-07 18:06:22 -0500

    F

---
+++ projects/first-steps/src/App.tsx
@@ -0,0 +1,16 @@
+export function App(){
+    return (
+        <article>
+            <header>
+                <img src="https://unavatar.io/kikobeats" />
+                <div>
+                    <p><strong>My personalizated avatar</strong></p>
+                    <span>@mendozasiiiuu</span>
+                </div>
+            </header>
+            <aside>
+                <button>Follow</button>
+            </aside>
+        </article>
+    )
+}
